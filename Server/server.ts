3462f6e6ee27d6b2620d8fe51734a9f263e91326
Praveen Kumar <praveenrajput1425@gmail.com>
2026-06-20 10:48:29 +0530

    backend server completed

--- Server/server.ts
+++ Server/server.ts
@@ -6,6 +6,8 @@ import authRouter from "./routes/authRoutes.js";
 import socialAuthRouter from "./routes/socialAuthRoutes.js";
 import accountRouter from "./routes/accountRoutes.js";
 import postRouter from "./routes/postRoutes.js";
+import activityRouter from "./routes/activityRoutes.js";
+import { initScheduler } from "./services/schedulerService.js";
 
 
 const app = express();
@@ -23,10 +25,14 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 
-app.use("/api/auth", authRouter);
-app.use("/api/oauth",socialAuthRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/oauth", socialAuthRouter)
 app.use("/api/accounts", accountRouter)
-app.use("/api/posts" , postRouter)
+app.use("/api/posts", postRouter)
+app.use("/api/activity", activityRouter)
+
+// Initialize Scheduler 
+initScheduler()
 
 // Global Error Handler
 app.use((err: any, _req: Request, res: Response , _next: NextFunction) =>{
